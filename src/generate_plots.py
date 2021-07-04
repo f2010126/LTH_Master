f8ce67caf83d9ce717ce5f98301eee743bcd4d16
Kashan Karim <mkashankarim@gmail.com>
2021-07-04 16:41:43 +0200

    added script to display plots from json

---
+++ src/generate_plots.py
@@ -0,0 +1,40 @@
+from utils import plot_graph
+import argparse
+import json
+import os
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='LTH Experiments')
+    parser.add_argument('--data-path', 
+                        help='path to data file',
+                        type=str,required=True)
+    args = parser.parse_args()
+    
+    assert os.path.exists(args.data_path),\
+        "File {} does not exits,\nPlease make sure file is accessible from here!".format(args.data_path)
+    with open(args.data_path) as f:
+        run_data = json.load(f)
+    file_name = args.data_path.split(os.path.sep)[-1]
+    epochs = max([y for x in run_data['prune_data'] for y in [x['rand_es'],x['pruned_es']]])
+    plot = {'title': file_name.split('.')[0],
+            'x_label': "Weights remaining",
+            'y_label': "Early Stop Epoch",
+            'baseline': "full_es",
+            'x_val': 'rem_weight',
+            'y_val': ['pruned_es', 'rand_es'],
+            'y_max': epochs+2,
+            'y_min': 'rand_es'}
+    plot_graph(run_data, plot, file_at=file_name + "_es.png",save_figure=False)
+    plot = {'title': file_name.split('.')[0],
+            'x_label': "Weights remaining",
+            'y_label': "Validation Accuracy",
+            'baseline': "val_score",
+            'x_val': 'rem_weight',
+            'y_val': ['val_score', 'rand_init'],
+            'y_max': 100,
+            'y_min': 'rand_init'}
+    plot_graph(run_data, plot, file_at=file_name + ".png",save_figure=False)
+    plt.show()
+    
--- src/utils.py
+++ src/utils.py
@@ -44,8 +44,8 @@ def save_data(json_dump, name_file="exp_result.json"):
     if not os.path.exists(json_path):
         os.makedirs(json_path)
     fname = os.path.join(json_path, name_file)
-    fh = open(fname, "w")
-    json.dump(json_dump, fh)
+    with open(fname, "w") as fh:
+        json.dump(json_dump, fh)
     return fname
 
 
@@ -53,7 +53,7 @@ def rounddown(x):
     return int(math.floor(x / 10.0)) * 10
 
 
-def plot_graph(graph_data, plot_config, file_at="pruned.png"):
+def plot_graph(graph_data, plot_config, file_at="pruned.png",save_figure=True):
     """
     Plots the graph. need a baseline and x,y points
     :param graph_data: dict of data
@@ -79,6 +79,8 @@ def plot_graph(graph_data, plot_config, file_at="pruned.png"):
     ax.set_xlabel(plot_config['x_label'])
     ax.set_ylabel(plot_config['y_label'])
     ax.legend(loc='best')
+    if  not save_figure:
+        return 
     fig = ax.get_figure()
     json_path = os.path.join(os.getcwd(), "LTH_Results")
     fig.savefig(os.path.join(json_path, file_at))
