38f41195fb462e5f19bc3e06266c15d75d42bf35
Dipti S <diptibits2010@gmail.com>
2021-07-11 09:49:41 +0200

    Observing ES in very sparse model

--- src/cheating_prune.py
+++ src/cheating_prune.py
@@ -59,7 +59,7 @@ def pruned(model, args):
     # rando_net = globals()[args.model](in_channels=in_chan)
     # rando_net.apply(init_weights)
     # set pruning configs
-    prune_amt = [0.1, 0.59, 0.83 ,0.93, 0.97, 0.995]
+    prune_amt = [0.97, 0.995]
     for amt in prune_amt:
         # Prune and get the new mask.
         with torch.no_grad():
