93f786e374bb2c99c73eb3773d4e15a354fcb277
Dipti S <diptibits2010@gmail.com>
2021-09-06 08:30:01 +0200

    Renamed a few files

--- src/run_pruning_experiment.py
+++ src/run_pruning_experiment.py
@@ -6,7 +6,7 @@ import copy
 from torchsummary import summary
 from linearnets import LeNet, LeNet300
 from convnets import Net2
-from run_model import run_training
+from run_model_experiment import run_training
 from prune_model import get_masks, update_apply_masks
 from prune_model import prune_random
 from utils import save_data, plot_graph
@@ -36,10 +36,14 @@ def handle_og_model(model, args):
     # get hold of w0
     all_masks = {key: mask.to(device) for key, mask in get_masks(model, prune_amts=LTH_Constants.init_mask)}
     original_state_dict = copy.deepcopy(model.state_dict())
-    # Run and train the lenet OG, done in run_model.py
+    # # incase loading happens
+    # model_checkpt = torch.load("mnist_lenet_OG.pth")
+    # model.load_state_dict(original_state_dict)
+    # Run and train the lenet OG, done in run_model_experiment.py
     metrics, full_es, _ = run_training(model, device, args=args)
-    # Save trained model
+    # Save OG model
     torch.save(model.state_dict(), "mnist_lenet_OG.pth")
+
     return original_state_dict, all_masks, {"val_score": metrics['val_score'] * 100,
                                             "full_es": full_es}
 
@@ -54,39 +58,36 @@ def pruned(model, args):
     original_state_dict, all_masks, baselines = handle_og_model(model, args)
     prune_data = []
     # init a random model
-    # in_chan = 1 if args.dataset == 'mnist' else 3
-    # rando_net = globals()[args.model](in_channels=in_chan)
-    # rando_net.apply(init_weights)
+    in_chan = 1 if args.dataset == 'mnist' else 3
+    rando_net = globals()[args.model](in_channels=in_chan)
+    rando_net.apply(init_weights)
     # set pruning configs
-    prune_amt = [0.1, 0.5, 0.6, 0.7, 0.97, 0.995]
-    for amt in prune_amt:
+    prune_amt = LTH_Constants.conv2_prune if args.model == 'Net2' else LTH_Constants.lenet_prune
+    for level in range(args.pruning_levels):
         # Prune and get the new mask.
         with torch.no_grad():
-            masks = get_masks(model, p_rate=amt)
+            prune_rate = args.pruning_rate / 100
+            masks = get_masks(model, prune_amts=prune_amt)
             # create a dict that has the same keys as state dict w/o being linked to model.
             detached = dict([(name, mask.clone().to(device)) for name, mask in masks])
             update_masks(all_masks, detached)
             # Load the OG weights and mask it
             model.load_state_dict(copy.deepcopy(original_state_dict))
-            # no need to apply masks i think?
-            # model = update_apply_masks(model, all_masks)
+            model = update_apply_masks(model, all_masks)
             # prune randomly inited model randomly
-            # init a random model
-            in_chan = 1 if args.dataset == 'mnist' else 3
-            rando_net = globals()[args.model](in_channels=in_chan)
-            rando_net.apply(init_weights)
-            prune_random(rando_net, amt)
+            prune_random(rando_net, prune_rate, prune_amts=prune_amt)
             non_zero = count_rem_weights(model)
-            print(f"Pruning amt {amt * 100} Weights remaining {non_zero} and 0% is {100 - non_zero}")
+            print(f"Pruning round {level + 1} Weights remaining {non_zero} and 0% is {100 - non_zero}")
         last_run, pruned_es, training = run_training(model, device, args=args)
-        rand_run, rand_es = {'val_score': 0}, 0
-        # rand_run, rand_es, _ = run_training(rando_net, device, args)
+        # rand_run, rand_es = {'val_score':0}, 0
+        rand_run, rand_es, rand_training = run_training(rando_net, device, args)
         prune_data.append({"rem_weight": non_zero,
                            "val_score": last_run['val_score'] * 100,
                            "rand_init": rand_run['val_score'] * 100,
                            "pruned_es": pruned_es,
                            "rand_es": rand_es,
-                           "training_data": training})
+                           "training_data": training,
+                           "random_training":rand_training})
     # metrics
     # TODO: baseline
     return baselines, prune_data
@@ -95,14 +96,14 @@ def pruned(model, args):
 if __name__ == '__main__':
     start = time.time()
     # Training settings
-    parser = argparse.ArgumentParser(description='Selectively Pruning to certain sparsity')
+    parser = argparse.ArgumentParser(description='LTH Experiments')
     parser.add_argument('--model', default='LeNet300',
                         help='Class name of model to train',
                         type=str, choices=['LeNet', 'Net2', 'LeNet300'])
     parser.add_argument('--batch-size', type=int, default=60,
                         help='input batch size for training (default: 60)')
 
-    parser.add_argument('--epochs', type=int, default=1,
+    parser.add_argument('--epochs', type=int, default=2,
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--iterations', type=int, default=50000,
                         help='number of iterations to train (default: 50000)')
@@ -110,13 +111,19 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, default=1.2e-3,
                         help='learning rate 1.2e-3')
 
+    parser.add_argument('--pruning-rate', type=int, default=20,
+                        help='how much to prune. taken as a % (default: 20)')
+
+    parser.add_argument('--pruning-levels', type=int, default=1,
+                        help='No. of times to prune (default: 3), referred to as levels in paper')
+
     parser.add_argument('--dataset', type=str, default='mnist', choices=['mnist', 'cifar10'],
                         help='Data to use for training')
     parser.add_argument('--early-stop',
                         action='store_true', help='Does Early if enabled')
-    parser.add_argument('--early-delta', type=float, default=0.005,
+    parser.add_argument('--early-delta', type=float, default=0.0005,
                         help='Difference b/w best and current to decide to stop early')
-    parser.add_argument('--name', default='cheating_prune',
+    parser.add_argument('--name', default='prune',
                         help='name to save data files and plots',
                         type=str)
     # prune to 30 to get 0.1% weights but 25 is ok too
@@ -134,7 +141,7 @@ if __name__ == '__main__':
     hours, rem = divmod(end - start, 3600)
     minutes, seconds = divmod(rem, 60)
     print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
-    file_name = f"{args.name}_{args.model}_{args.dataset}"
+    file_name = f"{args.name}_{args.model}_{args.dataset}_{args.pruning_levels}"
     stored_at = save_data(run_data, file_name + ".json")
     plot = LTH_Constants.default_plot_es
     plot['title'] = file_name
