441b0e820a7e65b41992246cde755905ba36fdcf
dsengupt <dsengupt@informatik.uni-freiburg.de>
2021-10-03 17:46:17 +0200

    Changing the order

--- Scripts/SLURM/prune_swa.sh
+++ Scripts/SLURM/prune_swa.sh
@@ -16,11 +16,13 @@ cd LTH_Master
 python3 -c "import torch; print(torch.__version__)"
 python3 -c "import torch; print(torch.cuda.is_available())"
 
-echo "Starting SWA for Net2"
-python3 -m src.vanilla_pytorch.shortcut_pruning --model Net2 --batch-size 60 --epochs 30 --lr 2e-4 --dataset cifar10 --use-swa --name Net2SWARun1
+
 
 echo "Start IMP for Net2"
 python3 -m src.vanilla_pytorch.shortcut_pruning --model Net2 --batch-size 60 --epochs 30 --lr 2e-4 --dataset cifar10 --name Net2_IMP_Run1
+
+echo "Starting SWA for Net2"
+python3 -m src.vanilla_pytorch.shortcut_pruning --model Net2 --batch-size 60 --epochs 30 --lr 2e-4 --dataset cifar10 --use-swa --name Net2SWARun1
 
 echo "Starting 2nd SWA for Resnet"
 python3 -m src.vanilla_pytorch.shortcut_pruning --model Resnets --batch-size 512 --epochs 30 --lr 0.01 --dataset cifar10 --use-swa --name ResNet_SWARun2
