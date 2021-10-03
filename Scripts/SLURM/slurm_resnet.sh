e5dc2909c18f848bb338732ba9eb275ba5865513
dsengupt <dsengupt@informatik.uni-freiburg.de>
2021-10-03 07:06:06 +0200

    minor changes for slurm

--- Scripts/SLURM/slurm_resnet.sh
+++ Scripts/SLURM/slurm_resnet.sh
@@ -3,7 +3,7 @@
 #SBATCH -e /work/dlclarge1/dsengupt-lth_ws/nemo_logs/full_res2.err # STDERR  (the folder log has to be created prior to running or this won't work)
 #SBATCH -J ResNet_Prune
 #SBATCH -N 1
-#SBATCH -t walltime=49:59:00
+#SBATCH -t 19:59:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=dipti.sengupta@students.uni-freiburg.de
 
@@ -19,6 +19,6 @@ cd LTH_Master
 python3 -c "import torch; print(torch.__version__)"
 python3 -c "import torch; print(torch.cuda.is_available())"
 
-python3 -m src.vanilla_pytorch.run_pruning_experiment --model Resnets --batch-size 512 --epochs 30 --lr 0.01 --pruning-levels 15 --dataset cifar10 --name ResnetRun2
+python3 -m src.vanilla_pytorch.run_pruning_experiment --model Resnets --batch-size 512 --epochs 30 --lr 0.01 --pruning-levels 15 --dataset cifar10 --name ResnetRun3
 
 deactivate
