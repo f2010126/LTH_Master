f192eb01483e134ca407a48560ab4674d14005b0
Dipti S <diptibits2010@gmail.com>
2021-10-03 17:30:17 +0200

    SWA ConvNet2

--- Scripts/SLURM/prune_swa.sh
+++ Scripts/SLURM/prune_swa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -o /work/dlclarge1/dsengupt-lth_ws/nemo_logs/all_swa.out # STDOUT  (the folder log has to be created prior to running or this won't work)
 #SBATCH -e /work/dlclarge1/dsengupt-lth_ws/nemo_logs/all_swa.err # STDERR  (the folder log has to be created prior to running or this won't work)
-#SBATCH -J ResNet_Prune
+#SBATCH -J Net2_Prune
 #SBATCH -N 1
 #SBATCH -t 19:59:00
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -16,14 +16,14 @@ cd LTH_Master
 python3 -c "import torch; print(torch.__version__)"
 python3 -c "import torch; print(torch.cuda.is_available())"
 
-echo "Starting SWA for Resnet"
-python3 -m src.vanilla_pytorch.shortcut_pruning --model Resnets --batch-size 512 --epochs 30 --lr 0.01 --dataset cifar10 --use-swa --name ResNet_SWARun1
-
 echo "Starting SWA for Net2"
-python3 -m src.vanilla_pytorch.run_pruning_experiment --model Net2 --batch-size 60 --epochs 30 --lr 2e-4 --pruning-levels 20 --dataset cifar10 --use-swa --name Net2SWARun1
+python3 -m src.vanilla_pytorch.shortcut_pruning --model Net2 --batch-size 60 --epochs 30 --lr 2e-4 --dataset cifar10 --use-swa --name Net2SWARun1
 
 echo "Start IMP for Net2"
-python3 -m src.vanilla_pytorch.run_pruning_experiment --model Net2 --batch-size 60 --epochs 30 --lr 2e-4 --pruning-levels 20 --dataset cifar10 --name Net2_IMP_Run1
+python3 -m src.vanilla_pytorch.shortcut_pruning --model Net2 --batch-size 60 --epochs 30 --lr 2e-4 --dataset cifar10 --name Net2_IMP_Run1
+
+echo "Starting 2nd SWA for Resnet"
+python3 -m src.vanilla_pytorch.shortcut_pruning --model Resnets --batch-size 512 --epochs 30 --lr 0.01 --dataset cifar10 --use-swa --name ResNet_SWARun2
 
 
 deactivate
