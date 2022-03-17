df1094ba355bb56d63e251ba0925c1d22635ca49
Dipti Sengupta <diptisengupa@Diptis-MBP-2.fritz.box>
2022-03-17 10:20:25 +0100

    script

---
+++ Scripts/SLURM/ssl_lth.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+#SBATCH -o /work/dlclarge2/dsengupt-lth_ws/slurm_logs/lth_ssl.out # STDOUT  (the folder log has to be created prior to running or this won't work)
+#SBATCH -e /work/dlclarge2/dsengupt-lth_ws/slurm_logs/lth_ssl.err # STDERR  (the folder log has to be created prior to running or this won't work)
+#SBATCH -J RepExp4_15
+#SBATCH -p mlhiwidlc_gpu-rtx2080
+#SBATCH --mem=0
+#SBATCH -q dlc-dsengupt
+#SBATCH -t 10:59:00
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=dipti.sengupta@students.uni-freiburg.de
+
+
+while getopts c: flag
+do
+    case "${flag}" in
+        c) config_name=${OPTARG};;
+    esac
+done
+
+export WANDB_START_METHOD=thread
+
+echo "# Config file: $config_name";
+
+cd $(ws_find lth_ws)
+# python3 -m venv lth_env
+source lth_env/bin/activate
+pip list
+cd LTH_Master
+# pick config file here
+python3 -m src.Lightning_WandB.prune_ssl --config_file_name $config_name
+
+deactivate
